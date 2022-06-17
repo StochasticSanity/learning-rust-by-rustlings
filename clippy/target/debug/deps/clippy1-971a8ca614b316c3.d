790ef3945a5f0490572891ce6b7b12e56a4c2610
Joseph Erdosy <106117424+StochasticSanity@users.noreply.github.com>
2022-06-17 11:44:56 -0400

    :fire: Finished: Section Four

---
+++ clippy/target/debug/deps/clippy1-971a8ca614b316c3.d
@@ -0,0 +1,7 @@
+/home/stochasticsanity/Projects/learning-rust-by-rustlings/exercises/clippy/target/debug/deps/clippy1-971a8ca614b316c3.rmeta: clippy1.rs
+
+/home/stochasticsanity/Projects/learning-rust-by-rustlings/exercises/clippy/target/debug/deps/clippy1-971a8ca614b316c3.d: clippy1.rs
+
+clippy1.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__-D__CLIPPY_HACKERY__clippy::float_cmp__CLIPPY_HACKERY__
